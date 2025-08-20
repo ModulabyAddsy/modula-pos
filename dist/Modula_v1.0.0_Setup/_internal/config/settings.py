f4a54990936a086a00c4233864a4a616e67b1384
Addsy <AddsyCloud@gmail.com>
2025-08-20 01:46:43 -0600

    Primera interaccion con la etapa de actualizaciones

---
+++ dist/Modula_v1.0.0_Setup/_internal/config/settings.py
@@ -0,0 +1,4 @@
+# src/config/settings.py
+
+# URL base del backend alojado en Render.
+API_BASE_URL = 'https://modula-backend.onrender.com'
--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import sys
 from PySide6.QtWidgets import QApplication
 from dotenv import load_dotenv
 import src.resources_rc
-
-import ctypes
-if sys.platform == 'win32':
-    myappid = 'addsy.modula.pos.1.0' # Un ID único para tu aplicación
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+from src.core.utils import resource_path
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
+# --- Importar el controlador principal ---
 from src.core.app_controller import AppController
 
+# --- Punto de entrada principal ---
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # --- Cargar hoja de estilos de forma segura ---
     try:
-        with open("src/assets/qss/style.qss", "r", encoding="utf-8") as f:
+        # Ahora la ruta relativa NO lleva 'src' al principio
+        stylesheet_path = resource_path("assets/qss/style.qss")
+        with open(stylesheet_path, "r", encoding="utf-8") as f:
             app.setStyleSheet(f.read())
     except FileNotFoundError:
         print("Advertencia: No se encontró el archivo de estilos 'style.qss'.")
 
+    # --- Iniciar la aplicación ---
     controller = AppController(app)
     controller.run()
     sys.exit(app.exec())
